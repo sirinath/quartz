58953ab6bed0ed138ba73d68fb3a867a7fad2b80
jvoegele <jvoegele@69f7d36a-ea1c-0410-88ea-9fd03e4c9665>
2011-11-10 20:06:55 +0000

    Rename to match conventions.

--- examples/example13/instance2.sh
+++ examples/example13/instance2.sh
@@ -2,28 +2,27 @@
 
 # Change this to your JDK installation root
 #
-#JAVA_HOME=/usr/java/jdk1.6.0_18
+#JAVA_HOME=/usr/java/j2sdk1.4.0_01
 
 JRE=$JAVA_HOME/jre
 JAVA=$JRE/bin/java
 
-workdir=`dirname $0`
-workdir=`cd ${workdir} && pwd`
-QUARTZ=${workdir}/../..
-
 . ${QUARTZ}/examples/bin/buildcp.sh
 
+#
 # Set the path to your JDCB Driver jar file here
-# or just drop the jar in the Quartz lib folder
-# JDBC_CP=/home/user/lib/postgres.jar
+JDBC_CP=/home/user/lib/postgres.jar
 
 QUARTZ_CP=$QUARTZ_CP:$JDBC_CP
 
 
-LOGGING_PROPS="-Dlog4j.configuration=file:${workdir}/log4j.xml"
+# Uncomment the following line if you would like to set log4j 
+# logging properties
+#
+#LOGGING_PROPS="-Dlog4j.configuration=log4j.properties"
 
 # Set the name and location of the quartz.properties file
-QUARTZ_PROPS="-Dorg.quartz.properties=${workdir}/instance2.properties"
+QUARTZ_PROPS="-Dorg.quartz.properties=instance2.properties"
 
 $JAVA -classpath $QUARTZ_CP $QUARTZ_PROPS $LOGGING_PROPS org.quartz.examples.example13.ClusterExample dontScheduleJobs
 
--- examples/example5/example5.sh
+++ examples/example5/example5.sh
@@ -2,18 +2,17 @@
 
 # Change this to your JDK installation root
 #
-#JAVA_HOME=/usr/java/jdk1.6.0_18
+#JAVA_HOME=/usr/java/j2sdk1.4.0_01
 
 JRE=$JAVA_HOME/jre
 JAVA=$JRE/bin/java
 
-workdir=`dirname $0`
-workdir=`cd ${workdir} && pwd`
-QUARTZ=${workdir}/../..
-
 . ${QUARTZ}/examples/bin/buildcp.sh
 
-LOGGING_PROPS="-Dlog4j.configuration=file:${workdir}/log4j.xml"
+# Uncomment the following line if you would like to set log4j 
+# logging properties
+#
+#LOGGING_PROPS="-Dlog4j.configuration=log4j.properties"
 
 $JAVA -classpath $QUARTZ_CP $LOGGING_PROPS org.quartz.examples.example5.MisfireExample
 
